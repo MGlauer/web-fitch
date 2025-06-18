c00c819d046e5f83c7cc70f8dc06151e887ade25
Martin Glauer <martinglauer89@gmail.com>
2025-06-18 15:02:13 +0200

    Remove toArray
    This function is not available in old-ish versions of safari
    
    Closes #32

--- src/components/FitchBox.jsx
+++ src/components/FitchBox.jsx
@@ -8,9 +8,9 @@ import {RuleError} from "../fitch/rules.js"
 export default function FitchBox() {
     const [premisesEnd, setPremisesEnd] = React.useState(0);
     const [lines, setLines] = React.useState([]);
-
-    const premises = lines.entries().toArray().slice(0, premisesEnd);
-    const proofLines = lines.entries().toArray().slice(premisesEnd);
+    const entries = [...lines.entries()]
+    const premises = entries.slice(0, premisesEnd);
+    const proofLines = entries.slice(premisesEnd);
 
     function alterRefs(justification, alter){
         if(justification.lines.processed) {
