730b7eb38b99568b408ccb0db476eed482fbafc7
MGlauer <martinglauer89@gmail.com>
2025-06-23 15:16:23 +0200

    Fix parser

--- test/parser/reader.test.js
+++ test/parser/reader.test.js
@@ -1,15 +1,30 @@
 import {describe, expect, it} from "vitest";
-import {parse, Atom, PropAtoms, Constant, BinarySentence, BinaryOp} from "../../src/fitch/structure.js";
+import {
+    parse,
+    Atom,
+    PropAtoms,
+    Constant,
+    BinarySentence,
+    BinaryOp,
+    QuantifiedSentence, Quantor, UnarySentence, UnaryOp, Variable
+} from "../../src/fitch/structure.js";
 
 
 const
     A = new PropAtoms("A"),
     B = new PropAtoms("B"),
     C = new PropAtoms("C"),
+
     and = (x,y) => new BinarySentence(x, BinaryOp.AND, y),
     or = (x,y) => new BinarySentence(x, BinaryOp.OR, y),
     lif = (x,y) => new BinarySentence(x, BinaryOp.IMPL, y),
-    liff = (x,y) => new BinarySentence(x, BinaryOp.BIMPL, y)
+    liff = (x,y) => new BinarySentence(x, BinaryOp.BIMPL, y),
+    not = (x) => new UnarySentence(UnaryOp.NEG, x),
+    all = (v,x) => new QuantifiedSentence(Quantor.ALL, v, x),
+    ex = (v,x) => new QuantifiedSentence(Quantor.Ex, v, x),
+    P = ([...xs]) => new Atom("P", xs),
+    x = new Variable("x")
+
 
 const cases = [
     ["A", A],
@@ -22,6 +37,7 @@ const cases = [
     ["A&(B|C)", and(A,or(B,C))],
     ["A|(B&C)", or(A,and(B,C))],
     ["A|(B|C)", or(A,or(B,C))],
+    ["~?xP(x)", not(ex(x.name,P([x])))]
 ]
 
 describe(`Reader test`, () => {
